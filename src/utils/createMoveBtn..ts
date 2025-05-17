f0529cdc90073126b5bf6fe409c6a97f90c2c52d
Yujiin <yjjeong0528@gmail.com>
2025-05-18 03:35:37 +0900

    feature : 이동버튼 UI 제작(추가수정필요)  #23

--- src/utils/createMoveBtn..ts
+++ src/utils/createMoveBtn..ts
@@ -1,15 +1,85 @@
+type MoveType = 'up' | 'down' | 'left' | 'right';
+
 interface MoveButtonOptions {
-  iconSrc: string; // 이미지 경로
   altText: string; // 대체 텍스트
   position: {
-    top?: string;
-    left?: string;
-    right?: string;
-    bottom?: string;
+    bottom: string;
+    right: string;
+    position: 'fixed' | 'absolute';
   };
-  id?: string; // 찾기 버튼 ID
+  id: string; // 방향키 버튼 ID
+  type: MoveType; // 이동 타입
+  isActive?: boolean; // 활성화 여부
   onClick: () => void; // 클릭 시 동작
+  moveText: string; //방향 텍스트
+  moveTextPosition: {
+    bottom: string;
+    right: string;
+    position: 'fixed' | 'absolute';
+  }; //방향 텍스트 위치
 }
 
-// 이동 버튼 생성자 함수
-class Movebutton {}
+// 이동 버튼 생성 클래스
+export class CreateMoveBtn {
+  //이동 버튼 만들기
+  private element: HTMLButtonElement;
+  private textElement: HTMLSpanElement;
+  private options: MoveButtonOptions;
+  private isActive: boolean = false; // 버튼 활성화 상태
+
+  constructor(options: MoveButtonOptions) {
+    this.options = options;
+    this.element = this.createButton();
+    this.textElement = this.createMoveText();
+    this.element.addEventListener('click', () => this.onClick());
+    this.isActive = options.isActive || false;
+    this.updateButtonState(); // 버튼 상태 업데이트
+  }
+
+  private createButton(): HTMLButtonElement {
+    const btn = document.createElement('button');
+    btn.setAttribute('aria-label', this.options.altText);
+    btn.id = this.options.id;
+    btn.className = 'move-btn-style';
+
+    // 버튼 위치 설정
+    Object.assign(btn.style, this.options.position);
+
+    // 이미지 삽입(인라인 svg - 아이콘 색상 채우기 문제)
+    btn.innerHTML = `
+  <svg class="move-icon-img rotate-${this.options.type} text-(--gray-11) hover:text-(--light-primary) active:text-(--light-primary)"
+       viewBox="0 0 148 189"
+       xmlns="http://www.w3.org/2000/svg">
+    <path d="M140.806 82.1038C149.545 88.0578 149.545 100.942 140.806 106.896L24.1959 186.346C14.2384 193.13 0.749991 185.999 0.749991 173.95L0.749998 15.0504C0.749999 3.00141 14.2384 -4.13014 24.1959 2.65418L140.806 82.1038Z"
+          fill="currentColor" />
+  </svg>
+`;
+
+    return btn;
+  }
+
+  // 이동 텍스트 만들기
+  private createMoveText(): HTMLSpanElement {
+    const moveText = document.createElement('span');
+    moveText.className = 'move-text-style';
+    moveText.innerText = this.options.moveText;
+
+    // 버튼 텍스트 위치 설정
+    Object.assign(moveText.style, this.options.moveTextPosition);
+
+    return moveText;
+  }
+
+  //중간에 동그란 원 UI 만들기
+
+  // 이동 버튼 클릭 핸들러
+  private onClick() {}
+  // 버튼 활성화 상태 업데이트
+  private updateButtonState() {}
+
+  // 버튼과 텍스트를 DOM에 추가
+  public appendTo(target: HTMLElement) {
+    target.appendChild(this.element);
+    document.body.appendChild(this.textElement);
+  }
+}
